c36f4df40326c04d96a9200e04fa4e2f4b0f07fa
AnisaHMohamed <anisahamohamed@gmail.com>
2022-07-02 15:20:09 -0400

    refractor: unused components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import "./App.css";
+import './App.css';
 import MenuPage from "./components/MenuPage";
-import { ChakraProvider, Box } from "@chakra-ui/react";
-
+import { ChakraProvider, Box } from "@chakra-ui/react"
 function App() {
   return (
+    <div className="App">
     <ChakraProvider>
-      <Box height="100%" backgroundColor="#262424">
-        <MenuPage />
-      </Box>
-    </ChakraProvider>
+    <Box height="100%" backgroundColor="#262424">
+      <MenuPage />
+    </Box>
+  </ChakraProvider>
+    </div>
   );
 }
 
 export default App;
+;
