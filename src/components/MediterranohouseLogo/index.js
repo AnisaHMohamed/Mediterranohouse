ed76b5e17c759893a3a4f0658c5f29e2fb240329
AnisaHMohamed <anisahamohamed@gmail.com>
2022-07-02 15:32:31 -0400

    feature: name and icons

--- src/components/MediterranohouseLogo/index.js
+++ src/components/MediterranohouseLogo/index.js
@@ -1,13 +1,13 @@
-import { Image, Box } from "@chakra-ui/react";
-import MediterranohouseLogoImage from "../../assets/MediterranhouseLogoImage.png";
+import { Image, Box } from '@chakra-ui/react'
+import MediterranohouseLogoImage from '../../assets/MediterranhouseLogoImage.png'
 const MediterranohouseLogo = () => {
   return (
     <>
-      <Box boxSize="sm">
-        <Image src={MediterranohouseLogoImage} alt="Dan Abramov" />
-      </Box>
+    <Box boxSize='sm'>
+    <Image src={MediterranohouseLogoImage} alt='Dan Abramov' />
+  </Box>   
     </>
-  );
-};
+  )
+}
 
-export default MediterranohouseLogo;
+export default MediterranohouseLogo
